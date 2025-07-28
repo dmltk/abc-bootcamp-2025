de74396df73a699be1c7ac51bc29bca365b8b0c2
jaewon choi <dorwkdidqks@gmail.com>
2025-07-28 11:40:07 +0900

    지난주 수업 내용

---
+++ 03-cli.py
@@ -0,0 +1,16 @@
+from dotenv import load_dotenv
+from ai import get_personality_analysis
+
+load_dotenv()
+
+print("# AI 관상가 입니다. 얼굴 특징을 입력해주시면 성격과 미래를 전망해드릴게요.")
+line=input("얼굴 특징 : ").strip()
+if not line:
+    print("에러: 얼굴 특징을 입력하지 않으셨습니다.")
+else:
+    print("입력 : ",line)
+    print("분석중...")
+    result=get_personality_analysis(line)
+    print("분석 완료!")
+    print(result)
+# get_personality_analysis(line)
