8a2f8383064537553322fa4944467d2fde59d282
jaewon choi <dorwkdidqks@gmail.com>
2025-07-29 09:44:28 +0900

    chat_tts

--- 06-cli-chat.py
+++ 06-cli-chat.py
@@ -3,22 +3,7 @@ from openai import OpenAI
 load_dotenv()
 client = OpenAI()
 
-converstions=[
-    # {"role":"system",
-    #  "content":"영어 상황극을 해보자. 스타벅스에서 커피를 구매. 너는 점원. 나는 손님"},
-    # {"role":"user","content":"대화를 시작해보자."}
-]
-
-# response = client.responses.create(
-#     model="gpt-4o",
-#     input=converstions,
-# )
-# assistant_content:str=response.output_text
-# print("[AI]",assistant_content)
-# converstions.append({
-#     "role":"assistant",
-#     "content":assistant_content
-# })
+converstions=[]
 while True:
     user_content=input("[Human] ").strip()
     if user_content:
@@ -39,3 +24,18 @@ while True:
     else:
         print("대화를 종료하겠습니다")
         break
+
+    # {"role":"system",
+    #  "content":"영어 상황극을 해보자. 스타벅스에서 커피를 구매. 너는 점원. 나는 손님"},
+    # {"role":"user","content":"대화를 시작해보자."}
+ 
+# response = client.responses.create(
+#     model="gpt-4o",
+#     input=converstions,
+# )
+# assistant_content:str=response.output_text
+# print("[AI]",assistant_content)
+# converstions.append({
+#     "role":"assistant",
+#     "content":assistant_content
+# })
